33a0130f35a45dc2a36cf5ba396af1961e980bfa
user104 <user104>
2017-07-04 11:27:09 +0200

    little adjustments

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 'use script';
 
-// var notes = [];
 
 window.onload = function() {
   getNotes();
   document.getElementById('addNote').onclick = function() {
     addNote();
   };
-  window.onresize = saveNotes;
   window.onbeforeunload = saveNotes;
 }
 
-function addNote(title = '', text = '') {
+function addNote(title = '', text = '', kleur = '') {
+  var kleuren = ['#b6bed6', '#b6ccaf', '#f4e8a1'];
   var note = document.createElement('li');
   var texten = document.createElement('div');
   var titel = document.createElement('textArea');
@@ -19,46 +18,40 @@ function addNote(title = '', text = '') {
   var img = document.createElement('img');
   titel.value = title;
   inhoud.value = text;
-  note.id = document.getElementsByTagName('ul')[0].children.length;
   img.src = 'images/close.png';
   img.className = 'closeNote';
-  img.onclick = closeNote;
-  // titel.onblur = function() {
-  //   notes[this.parentNode.parentNode.id]['titel'] = this.value;
-  // };
-  // inhoud.onblur = function() {
-  //   notes[this.parentNode.parentNode.id]['inhoud'] = this.value;
-  // };
+  img.onclick = function() {
+    var note = this.parentNode;
+    note.parentNode.removeChild(note);
+  }
+  note.style.backgroundColor = kleur || kleuren[Math.floor(Math.random() * kleuren.length)];
   texten.appendChild(titel);
   texten.appendChild(inhoud);
   note.appendChild(texten);
   note.appendChild(img);
   document.getElementsByTagName('ul')[0].appendChild(note);
   titel.focus();
-  // notes[note.id] = {titel: titel.value, inhoud: inhoud.value};
 }
 
-function closeNote() {
-  var note = this.parentNode;
-  // notes.splice(note.id, 1);
-  this.parentNode.parentNode.removeChild(this.parentNode);
-}
+
 
 
 
 function getNotes() {
-    var notes = JSON.parse(localStorage.getItem('notes')) || [];
-    notes.forEach(function(note) {
-      addNote(note.titel, note.inhoud);
-    });
+  var notes = JSON.parse(localStorage.getItem('notes')) || [];
+  notes.forEach(function(note) {
+    addNote(note.titel, note.inhoud, note.kleur);
+  });
 }
 
 function saveNotes() {
-  var notes = document.getElementsByTagName('li').
-  notes = Array.prototype.map.call(notes, function(note) {
+  var notes = document.getElementsByTagName('li');
+  notes = Array.prototype.slice.call(notes);
+  notes = notes.map(function(note) {
     return {
-      titel: note.firstChild.firstChild.textContent,
-      inhoud: note.firstChild.lastChild.textContent
+      titel: note.firstChild.firstChild.value,
+      inhoud: note.firstChild.lastChild.value,
+      kleur: note.style.backgroundColor
     };
   });
   localStorage.setItem('notes', JSON.stringify(notes));
